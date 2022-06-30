bcab8212564faa4142592b906dff3989ce78bfb4
Joshua Shaola <sshaola@gmail.com>
2022-06-30 16:17:18 -0700

    my first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,17 @@
-/* File-name: 4-puts.c 
+/*
+ *File-name: 4-puts.c
  * Author: Shaola Joshua
  */
 
 #include <stdio.h>
 /*
  * main- Print exactly "Programming is like building a multilingual
- *                	puzzle", followed by a new line.
+ * 			puzzle", followed by a new line.
  *
  * Return: Always 0 for main
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
