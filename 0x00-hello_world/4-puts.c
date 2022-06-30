2a9cc70dad5fb34243cca2fb7747d84b2ab41d9e
Joshua Shaola <sshaola@gmail.com>
2022-06-30 16:59:43 -0700

    fixing errors in file 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
 
 #include <stdio.h>
 /*
- * main: Prints "Programming is like building a multilingual
+ * main - Prints "Programming is like building a multilingual
  *			puzzle", followed by a new line.
  *
- * Return: Always 0 for main.
+ * Return: 0 for main.
  */
 int main(void)
 {
