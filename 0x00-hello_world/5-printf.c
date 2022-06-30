53f4da8d78a323f33a50f973566a35b2fa3f12f1
Joshua Shaola <sshaola@gmail.com>
2022-06-30 16:27:09 -0700

    using printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File-name: 5-printf.c
+ * Author: Shaola Joshua
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *		a piece of art," followed by a new line.
+ *
+ * Return: Always 0 for main.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+}
