179e96e1aa7d74afc47033bf2d02a8567a00bfce
Joshua Shaola <sshaola@gmail.com>
2022-07-12 14:52:08 -0700

    copying a pointed string to buffer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a pointed string to a buffer
+ * @str: pointed string
+ * @dest: buffer
+ *
+ * Return: The pointer to dest
+ */
+char *_strcpy(char *dest, char *str)
+{
+int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
