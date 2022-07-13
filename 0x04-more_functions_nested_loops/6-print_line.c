d078534abf5739c50f4ec5223df7c69255d1852c
Joshua Shaola <sshaola@gmail.com>
2022-07-13 13:10:21 -0700

    printing a line across the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times 'underscore' should be printed
+ *
+ * Return: Void
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
