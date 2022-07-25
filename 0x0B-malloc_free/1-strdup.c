4d66b1f75939ef955090717804093ece201942e0
Joshua Shaola <sshaola@gmail.com>
2022-07-25 15:17:37 -0700

    Allocating memory to the copy of a string as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup - A pointer to a newly allocated space in memory
+ *		with a copy of the string as a parameter.
+ * @str: string operand
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
